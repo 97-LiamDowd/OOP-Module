2efc8a7374d2401a4d587c8518a34f2ad5d5d3ae
97.LiamDowd <liam.jonathan.dowd@gmail.com>
2026-01-27 11:42:12 +0000

    keep forgetting to save the file first!

--- OOP Module - Java/Lotto.java
+++ OOP Module - Java/Lotto.java
@@ -6,13 +6,19 @@ public class Lotto {
         //Generate 6 random numbers
         for(int i = 0; i < lottoNums.length; i++){
             lottoNums[i] = (int)(Math.random() * 47) + 1;
+            for(int j = 0; j < i; j++){
+                if(lottoNums[i] == lottoNums[j]){
+                    i--; //duplicate found, redo this iteration
+                    break;
+                }
+            }
         }
 
         //print 6  numbers
         for(int i = 0; i < lottoNums.length; i++){
             System.out.println(lottoNums[i]);
         }
-
+        
     }
 
 }
