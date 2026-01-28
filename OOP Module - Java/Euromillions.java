b604a30c8200522c3dcaa280c1d4a12073ebf5aa
97.LiamDowd <liam.jonathan.dowd@gmail.com>
2026-01-28 13:03:57 +0000

    Another one

---
+++ OOP Module - Java/Euromillions.java
@@ -0,0 +1,24 @@
+public class Euromillions {
+    public static void main(String[] args){
+
+        int[] numbers = new int[5];
+        int[] luckystars = new int[2];
+
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = (int)(Math.random() *50)+1;
+            for(int j =0; j < i; j++){
+                if(numbers[i] == numbers[j]){
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        luckystars[0] = (int)(Math.random() * 12) + 1;
+        do {
+            luckystars[1] = (int)(Math.random() * 12) + 1;
+
+        } while (luckystars[0]  == luckystars[1]);
+
+    }
+}
