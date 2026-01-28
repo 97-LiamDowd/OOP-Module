b2aafffa54acda47346aaa514668c45c2f583b0b
97.LiamDowd <liam.jonathan.dowd@gmail.com>
2026-01-28 15:29:19 +0000

    another one

--- OOP Module - Java/BubbleSort.java
+++ OOP Module - Java/BubbleSort.java
@@ -29,7 +29,7 @@ public class BubbleSort {
                 }
             }
 
-            // Now this is in the RIGHT place
+            
             if (!swapped) {
                 break;
             }
