4b3333e222ca9e5232b36496e816d3ceda42804d
97.LiamDowd <liam.jonathan.dowd@gmail.com>
2026-01-28 14:30:27 +0000

    another one

--- OOP Module - Java/BubbleSort.java
+++ OOP Module - Java/BubbleSort.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 public class BubbleSort{
     public static void main(String[] args){
 
@@ -6,16 +5,40 @@ public class BubbleSort{
         long startTime;
         long duration;
 
-        startTime = System.currentTimeMillis();
-
-        //fill array with 200 random numbers between 1-200
+        //fill array with 200 random numbers between 1-100
         for (int i = 0; i < numbers.length; i++){
             numbers[i] = (int)(Math.random() * 100) + 1;
         }
 
-        System.out.println(Arrays.toString(numbers));
+        startTime = System.currentTimeMillis();
+        //bubble sort
+        
+        int temp; //hold number for swap
+        boolean swapped = false;
+        for (int i = 0; i < numbers.length; i++){
+            swapped = false;
+            for (int j = 0; j < numbers.length-1; j++){
+                if (numbers[j] > numbers[j+1]);
+                    
+                    //swap values
+                    temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                    swapped = true;
+            
+                }
 
+                System.out.println(numbers);
+
+             if (swapped == false){
+            break;
+            }   
+    
+        }
+        
         duration = System.currentTimeMillis() - startTime;
         System.out.println("Time in miliseconds: " + duration);
+        
+
     }
 }
