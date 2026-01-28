3a193d526a091fb588ec4a45792f91360364f4ce
97.LiamDowd <liam.jonathan.dowd@gmail.com>
2026-01-28 14:47:09 +0000

    Changed it so it would sort

--- OOP Module - Java/BubbleSort.java
+++ OOP Module - Java/BubbleSort.java
@@ -1,45 +1,43 @@
-public class BubbleSort{
-    public static void main(String[] args){
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
 
         int[] numbers = new int[200];
         long startTime;
         long duration;
 
-        //fill array with 200 random numbers between 1-100
-        for (int i = 0; i < numbers.length; i++){
+        // Fill array with 200 random numbers between 1-100
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int)(Math.random() * 100) + 1;
         }
 
         startTime = System.currentTimeMillis();
-        //bubble sort
-        
-        int temp; //hold number for swap
-        boolean swapped = false;
-        for (int i = 0; i < numbers.length; i++){
+
+        int temp;
+        boolean swapped;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
             swapped = false;
-            for (int j = 0; j < numbers.length-1; j++){
-                if (numbers[j] > numbers[j+1]){
-                    
-                    //swap values
+
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
                     temp = numbers[j];
-                    numbers[j] = numbers[j+1];
-                    numbers[j+1] = temp;
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
                     swapped = true;
-            
                 }
+            }
 
-            if (swapped == false){
+            // Now this is in the RIGHT place
+            if (!swapped) {
                 break;
-            }   
-
-            }   
-    
+            }
         }
-        
-        duration = System.currentTimeMillis() - startTime;
-        System.out.println("Time in miliseconds: " + duration);
-        System.out.println(numbers);
-        
 
+        duration = System.currentTimeMillis() - startTime;
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Time in milliseconds: " + duration);
     }
 }
